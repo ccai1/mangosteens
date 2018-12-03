17f2e5946c3ca69e09a3a24c3e7e136ac542a3c3
StefanTan18 <stan3@stuy.edu>
2018-12-03 12:37:45 -0500

    initialized spotify

---
+++ util/spotify.py
@@ -0,0 +1,7 @@
+import json
+from urllib import request, parse
+
+client_id = "77cf833ba97d4dbfbbd887ba64c6c73c"
+client_secret = "3f9ac93496244f04a95b6a4a216dc951"
+redirect_uri = "https://accounts.spotify.com/api/token"
+
