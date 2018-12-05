eb2ee25248c59c4239cc2597ffafebe18929806d
Angela <atom@stuy.edu>
2018-12-05 16:23:13 -0500

    Finished helper methods for playlist algo

--- util/music.py
+++ util/music.py
@@ -25,7 +25,7 @@ def get_track_info(artist, track):
 Returns duration of the track
 '''
 def get_track_duration(data):
-    return data["track"]["duration"]
+    return int(data["track"]["duration"]) / 1000
 
 '''
 Get a certain number of top tracks on chart
@@ -48,6 +48,26 @@ def get_top_tracks(num):
     return track_list
 
 '''
+Fixing track list; removes tracks with 0 duration
+Returns a new track list with only working tracks
+'''
+def fix_track_list(track_list):
+    print("Track List:")
+    print(track_list)
+    tracks = []
+    for track in track_list:
+        artist = track[0]
+        name = track[1]
+        try:
+            if get_track_duration(get_track_info(artist, name)) > 0:
+                tracks.append(track)
+        except:
+            print("Fixed")
+    print(tracks)
+    return tracks
+
+
+'''
 Get top tracks by tags
 Returns a list of artist + track name (same as get_top_tracks(num))
 '''
@@ -65,9 +85,30 @@ def get_tracks(tag, num):
         track_list.append([artist, track_name])
     return track_list
 
+
+
 print(get_top_tracks(5))
-print(get_tracks("edm", 3))
+print(get_tracks("holidays", 3))
+print(get_track_duration(get_track_info("Alan Walker", "Faded")))
 
-def find_playlist (time, mood = None, genre = None, artist = None):
+'''
+Returns the total duration of a track list
+'''
+def get_total_time(track_list):
+    total_time = 0
+    track_list = fix_track_list(track_list)
+    for track in track_list:
+            artist = track[0]
+            name = track[1]
+            total_time += get_track_duration(get_track_info(artist, name))
+    return total_time
 
-    return time
+print("Time : " + str(get_total_time(get_tracks("edm", 10))))
+
+'''
+The playlist maker method
+Returns a track list as per the tags or top charts if tags are not giving that will add up to total time
+'''
+def gen_playlist (time, tag, isTagged):
+    track_list = []
+    return track_list
